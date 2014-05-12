3974cb580f4c1ac331a96d41ad26ef99d399b618
Peter Markou <markoupetr@gmail.com>
2014-05-12 13:20:33 +0300

    Generate scaffold for Product

---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :title, :description, :price, :units, :created_at, :updated_at
---
+++ db/migrate/20140512101913_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.decimal :price
+      t.integer :units
+
+      t.timestamps
+    end
+  end
+end
