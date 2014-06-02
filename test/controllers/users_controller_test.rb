ffce620d431432af28d0d315567d6dd76b1d2c85
Peter Markou <markoupetr@gmail.com>
2014-06-02 21:29:10 +0300

    Update controllers tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+    @user = users(:peter)
   end
 
   test "should get index" do
@@ -18,10 +18,24 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { age: @user.age, gender: @user.gender, name: @user.name }
+      post :create, user: {
+        age: 23,
+        gender: 0,
+        name: "petrakos",
+        email_attributes: {
+          address: "petrakos@gmail.com"
+        }
+      }
     end
 
-    assert_redirected_to user_path(assigns(:user))
+    user = assigns(:user)
+
+    assert_redirected_to user_path(user)
+    assert_equal "petrakos", user.name
+    assert_equal 23, user.age
+    assert_equal 0, user.gender
+    assert_equal "petrakos@gmail.com", user.email.address
+    assert_equal "User: #{user.name} was successfully created.", flash[:notice]
   end
 
   test "should show user" do
@@ -35,8 +49,21 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { age: @user.age, gender: @user.gender, name: @user.name }
-    assert_redirected_to user_path(assigns(:user))
+    patch :update, id: @user, user: {
+      age: @user.age,
+      gender: @user.gender,
+      name: "petrakos",
+      email_attributes: {
+        address: "petrakos@gmail.com"
+      }
+    }
+
+    user = assigns(:user)
+
+    assert_redirected_to user_path(user)
+    assert_equal "petrakos", user.name
+    assert_equal "petrakos@gmail.com", user.email.address
+    assert_equal "User: #{user.name} was successfully updated.", flash[:notice]
   end
 
   test "should destroy user" do
