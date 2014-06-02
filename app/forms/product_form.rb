bd4f5bb05e318c3976cb1b58dcce2641839524cb
Peter Markou <markoupetr@gmail.com>
2014-06-02 20:48:20 +0300

    Delete the old Product scaffold

--- app/forms/product_form.rb
+++
@@ -1,6 +0,0 @@
-require "form_object"
-
-class ProductForm < FormObject::Base
-  attributes :title, :price, :units, :description
-  validates :title, :price, :units, :description, presence: true
-end
--- test/forms/compliance_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ComplianceTest < ActiveSupport::TestCase
-  include ActiveModel::Lint::Tests
-
-  def setup
-    @model = ProductForm.new(Product.new)
-  end
-end
