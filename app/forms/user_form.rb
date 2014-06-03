d31b3f3f9d39b8e85e8c1f6dc3f257d375435b15
Peter Markou <markoupetr@gmail.com>
2014-06-03 10:27:25 +0300

    UserForm contain both models, User and Email, and delegates the required form attributes to the appropriate model.

--- app/forms/user_form.rb
+++ app/forms/user_form.rb
@@ -1,9 +1,11 @@
 class UserForm
-  attr_reader :user
+  attr_reader :user, :email
   
+  delegate :address, :address=, to: :email
   delegate :name, :name=, :gender, :gender=, :age, :age=, to: :user
 
-  def initialize(user)
+  def initialize(user, email)
     @user = user
+    @email = email
   end
 end
--- test/forms/user_form_test.rb
+++ test/forms/user_form_test.rb
@@ -3,14 +3,15 @@ require 'test_helper'
 class UserFormTest < ActiveSupport::TestCase
   def setup
     @user = User.new
-    @user_form = UserForm.new(@user)
+    @email = Email.new
+    @user_form = UserForm.new(@user, @email)
   end
 
-  test "accepts the model it represents" do
+  test "accepts the User model" do
     assert_equal @user, @user_form.user
   end
 
-  test "responds to the model attributes" do
+  test "responds to the User attributes" do
     attributes = [:name, :name=, :age, :age=, :gender, :gender=]
 
     attributes.each do |attribute|
@@ -18,7 +19,7 @@ class UserFormTest < ActiveSupport::TestCase
     end
   end
 
-  test "delegate the attributes to its model" do
+  test "delegate the attributes to User" do
     @user_form.name = "Peter"
     @user_form.age = 23
     @user_form.gender = 0
@@ -26,5 +27,23 @@ class UserFormTest < ActiveSupport::TestCase
     assert_equal "Peter", @user.name
     assert_equal 23, @user.age
     assert_equal 0, @user.gender
+  end
+
+  test "accepts the Email model" do
+    assert_equal @email, @user_form.email
+  end
+
+  test "responds to the Email attributes" do
+    attributes = [:address, :address=]
+
+    attributes.each do |attribute|
+      assert_respond_to @user_form, attribute
+    end
+  end
+
+  test "delegates the attributes to Email" do
+    @user_form.address = "petrakos@gmail.com"
+
+    assert_equal "petrakos@gmail.com", @email.address
   end
 end
