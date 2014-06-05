9d41ebb758180f9c3a16a23e690bb32fd665b0c1
Peter Markou <markoupetr@gmail.com>
2014-06-05 20:13:26 +0300

    Add ability to create attributes of the form.

--- test/forms/nested_form_test.rb
+++ test/forms/nested_form_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class UserFormFixture
   attr_reader :model
+  attr_accessor :name, :age, :gender
 
   def initialize(model)
     @model = model
@@ -10,10 +11,20 @@ end
 
 class NestedFormTest < ActiveSupport::TestCase
 
-  test "accepts the model it represents" do
-    user = User.new
-    user_form = UserFormFixture.new(user)
+  def setup
+    @user = User.new
+    @user_form = UserFormFixture.new(@user)
+  end
 
-    assert_equal user, user_form.model
+  test "accepts the model it represents" do
+    assert_equal @user, @user_form.model
+  end
+
+  test "declares attributes for the model" do
+    attributes = [:name, :name=, :age, :age=, :gender, :gender=]
+
+    attributes.each do |attribute|
+      assert_respond_to @user_form, attribute
+    end
   end
 end
