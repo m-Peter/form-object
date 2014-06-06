500582f358f897a60395f25b758504f32c094feb
Peter Markou <markoupetr@gmail.com>
2014-06-06 15:01:33 +0300

    Migrate the database to include the Profile model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602175017) do
+ActiveRecord::Schema.define(version: 20140606115914) do
 
   create_table "emails", force: true do |t|
     t.string   "address"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 20140602175017) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "profiles", force: true do |t|
+    t.string   "twitter_name"
+    t.string   "github_name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "name"
