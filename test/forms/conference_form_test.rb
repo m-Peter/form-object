50e664b5d2819f44085f6c73779062f9c7a4944d
Peter Markou <markoupetr@gmail.com>
2014-06-20 23:06:09 +0300

    AbstractForm#save saves the nested object graph.

--- test/forms/conference_form_test.rb
+++ test/forms/conference_form_test.rb
@@ -123,4 +123,43 @@ class ConferenceFormTest < ActiveSupport::TestCase
     assert_equal "1h", @form.speaker.presentations[1].duration
     assert_equal 2, @form.speaker.presentations.size
   end
+
+  test "collection sub-form saves all the models" do
+    params = {
+      name: "Euruco",
+      city: "Athens",
+
+      speaker_attributes: {
+        name: "Peter Markou",
+        occupation: "Developer",
+
+        presentations_attributes: {
+          "0" => { topic: "Ruby OOP", duration: "1h" },
+          "1" => { topic: "Ruby Closures", duration: "1h" },
+        }
+      }
+    }
+
+    @form.submit(params)
+
+    assert_difference(['Conference.count', 'Speaker.count']) do
+      @form.save
+    end
+
+    assert_equal "Euruco", @form.name
+    assert_equal "Athens", @form.city
+    assert_equal "Peter Markou", @form.speaker.name
+    assert_equal "Developer", @form.speaker.occupation
+    assert_equal "Ruby OOP", @form.speaker.presentations[0].topic
+    assert_equal "1h", @form.speaker.presentations[0].duration
+    assert_equal "Ruby Closures", @form.speaker.presentations[1].topic
+    assert_equal "1h", @form.speaker.presentations[1].duration
+    assert_equal 2, @form.speaker.presentations.size
+
+    assert @form.persisted?
+    assert @form.speaker.persisted?
+    @form.speaker.presentations.each do |presentation|
+      assert presentation.persisted?
+    end
+  end
 end
