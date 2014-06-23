c8fd4c5518ba92705c05efdc8b16c172fd0194b4
Peter Markou <markoupetr@gmail.com>
2014-06-23 14:04:11 +0300

    Place the email_form variable in the setup method, so we don't have to assign in every test method.

--- test/forms/nested_model_form_test.rb
+++ test/forms/nested_model_form_test.rb
@@ -7,6 +7,7 @@ class NestedModelFormTest < ActiveSupport::TestCase
   def setup
     @user = User.new
     @form = UserWithEmailFormFixture.new(@user)
+    @email_form = @form.email
     @model = @form
   end
 
@@ -24,24 +25,20 @@ class NestedModelFormTest < ActiveSupport::TestCase
     assert_equal :email, email_definition.assoc_name
   end
 
-  test "email sub-form contains association name and parent model" do
-    email_form = @form.email
-
-    assert_equal :email, email_form.association_name
-    assert_equal @user, email_form.parent
-  end
-
   test "contains getter for email sub-form" do
     assert_respond_to @form, :email
     assert_instance_of Form, @form.email
   end
 
-  test "email sub-form initializes model for new parent" do
-    email_form = @form.email
+  test "email sub-form contains association name and parent model" do
+    assert_equal :email, @email_form.association_name
+    assert_equal @user, @email_form.parent
+  end
 
-    assert_instance_of Email, email_form.model
-    assert_equal @form.model.email, email_form.model
-    assert email_form.model.new_record?
+  test "email sub-form initializes model for new parent" do
+    assert_instance_of Email, @email_form.model
+    assert_equal @form.model.email, @email_form.model
+    assert @email_form.model.new_record?
   end
 
   test "email sub-form fetches model for existing parent" do
@@ -51,63 +48,56 @@ class NestedModelFormTest < ActiveSupport::TestCase
 
     assert_instance_of Email, email_form.model
     assert_equal user_form.model.email, email_form.model
-    assert email_form.model.persisted?
+    assert email_form.persisted?
+
     assert_equal "m-peter", user_form.name
     assert_equal 23, user_form.age
     assert_equal 0, user_form.gender
-    assert_equal "markoupetr@gmail.com", email_form.model.address
+    assert_equal "markoupetr@gmail.com", email_form.address
   end
 
   test "#represents? returns true if the argument matches the Form's association name, false otherwise" do
-    email_form = @form.email
-
-    assert email_form.represents?("email")
-    assert_not email_form.represents?("profile")
+    assert @email_form.represents?("email")
+    assert_not @email_form.represents?("profile")
   end
 
   test "email sub-form declares attributes" do
-    email_form = @form.email
-    attributes = [:address, :address=]
-
-    attributes.each do |attribute|
-      assert_respond_to email_form, attribute
+    [:address, :address=].each do |attribute|
+      assert_respond_to @email_form, attribute
     end
   end
 
   test "email sub-form delegates attributes to model" do
-    email_form = @form.email
-    email_form.address = "petrakos@gmail.com"
+    @email_form.address = "petrakos@gmail.com"
 
-    assert_equal "petrakos@gmail.com", email_form.address
-    assert_equal "petrakos@gmail.com", email_form.model.address
+    assert_equal "petrakos@gmail.com", @email_form.address
+    assert_equal "petrakos@gmail.com", @email_form.model.address
   end
 
   test "email sub-form validates itself" do
-    email_form = @form.email
-    email_form.address = nil
+    @email_form.address = nil
 
-    assert_not email_form.valid?
-    assert_includes email_form.errors.messages[:address], "can't be blank"
+    assert_not @email_form.valid?
+    assert_includes @email_form.errors.messages[:address], "can't be blank"
 
-    email_form.address = "petrakos@gmail.com"
+    @email_form.address = "petrakos@gmail.com"
 
-    assert email_form.valid?
+    assert @email_form.valid?
   end
 
   test "email sub-form validates the model" do
     existing_email = emails(:peters)
-    email_form = @form.email
-    email_form.address = existing_email.address
+    @email_form.address = existing_email.address
     
-    assert_not email_form.valid?
-    assert_includes email_form.errors.messages[:address], "has already been taken"
+    assert_not @email_form.valid?
+    assert_includes @email_form.errors.messages[:address], "has already been taken"
 
-    email_form.address = "petrakos@gmail.com"
+    @email_form.address = "petrakos@gmail.com"
 
-    assert email_form.valid?
+    assert @email_form.valid?
   end
 
-  test "main form syncs model in email sub-form" do
+  test "main form syncs its model and the models in nested sub-forms" do
     params = {
       name: "Petrakos",
       age: "23",
@@ -123,10 +113,10 @@ class NestedModelFormTest < ActiveSupport::TestCase
     assert_equal "Petrakos", @form.name
     assert_equal 23, @form.age
     assert_equal 0, @form.gender
-    assert_equal "petrakos@gmail.com", @form.email.address
+    assert_equal "petrakos@gmail.com", @email_form.address
   end
 
-  test "main form saves all the models" do
+  test "main form saves its model and the models in nested sub-forms" do
     params = {
       name: "Petrakos",
       age: "23",
@@ -146,10 +136,10 @@ class NestedModelFormTest < ActiveSupport::TestCase
     assert_equal "Petrakos", @form.name
     assert_equal 23, @form.age
     assert_equal 0, @form.gender
-    assert_equal "petrakos@gmail.com", @form.email.address
+    assert_equal "petrakos@gmail.com", @email_form.address
     
     assert @form.persisted?
-    assert @form.email.persisted?
+    assert @email_form.persisted?
   end
 
   test "main form collects all the model related errors" do
