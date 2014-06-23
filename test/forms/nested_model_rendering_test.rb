35461eac34a3e79f9e787763cc8ef19c2e474200
Peter Markou <markoupetr@gmail.com>
2014-06-23 14:18:21 +0300

    Add tests to check the rendering of a UserWithEmailFormFixture for the new and edit templates.

--- test/forms/nested_model_rendering_test.rb
+++ test/forms/nested_model_rendering_test.rb
@@ -3,6 +3,7 @@ require_relative 'user_with_email_and_profile_form_fixture'
 require_relative 'project_with_tasks_form_fixture'
 require_relative 'songs_form_fixture'
 require_relative 'user_form_fixture'
+require_relative 'user_with_email_form_fixture'
 
 class NestedModelRenderingTest < ActionView::TestCase
   def form_for(*)
@@ -77,6 +78,95 @@ class NestedModelRenderingTest < ActionView::TestCase
     assert_match /<option selected="selected" value="0">Male<\/option>/, output_buffer
     assert_match /<option value="1">Female<\/option>/, output_buffer
     assert_match /<\/select>/, output_buffer
+
+    assert_match /<input name="commit" type="submit" value="Update User" \/>/, output_buffer
+  end
+
+  test "form_for renders correctly a new instance of UserWithEmailFormFixture" do
+    user = User.new
+    user_form = UserWithEmailFormFixture.new(user)
+
+    form_for user_form do |f|
+      concat f.label(:name)
+      concat f.text_field(:name)
+
+      concat f.label(:age)
+      concat f.number_field(:age)
+
+      concat f.label(:gender)
+      concat f.select(:gender, User.get_genders_dropdown)
+
+      concat f.fields_for(:email, user_form.email) { |email_fields|
+        concat email_fields.label(:address)
+        concat email_fields.text_field(:address)
+      }
+
+      concat f.submit
+    end
+
+    assert_match /action="\/users"/, output_buffer
+    assert_match /class="new_user"/, output_buffer
+    assert_match /id="new_user"/, output_buffer
+    assert_match /method="post"/, output_buffer
+
+    assert_match /<label for="user_name">Name<\/label>/, output_buffer
+    assert_match /<input id="user_name" name="user\[name\]" type="text" \/>/, output_buffer
+    assert_match /<label for="user_age">Age<\/label>/, output_buffer
+    assert_match /input id="user_age" name="user\[age\]" type="number" \/>/, output_buffer
+    assert_match /<label for="user_gender">Gender<\/label>/, output_buffer
+    assert_match /<select id="user_gender" name="user\[gender\]">/, output_buffer
+    assert_match /<option value="0">Male<\/option>/, output_buffer
+    assert_match /<option value="1">Female<\/option>/, output_buffer
+    assert_match /<\/select>/, output_buffer
+
+    assert_match /<label for="user_email_attributes_address">Address<\/label>/, output_buffer
+    assert_match /<input id="user_email_attributes_address" name="user\[email_attributes\]\[address\]" type="text" \/>/, output_buffer
+
+    assert_match /<input name="commit" type="submit" value="Create User" \/>/, output_buffer
+  end
+
+  test "form_for renders correctly a existing instance of UserWithEmailFormFixture" do
+    user = users(:peter)
+    user_form = UserWithEmailFormFixture.new(user)
+
+    form_for user_form do |f|
+      concat f.label(:name)
+      concat f.text_field(:name)
+
+      concat f.label(:age)
+      concat f.number_field(:age)
+
+      concat f.label(:gender)
+      concat f.select(:gender, User.get_genders_dropdown)
+
+      concat f.fields_for(:email, user_form.email) { |email_fields|
+        concat email_fields.label(:address)
+        concat email_fields.text_field(:address)
+      }
+
+      concat f.submit
+    end
+
+    id = user.id
+
+    assert_match /action="\/users\/#{id}"/, output_buffer
+    assert_match /class="edit_user"/, output_buffer
+    assert_match /id="edit_user_#{id}"/, output_buffer
+    assert_match /method="post"/, output_buffer
+
+    assert_match /<label for="user_name">Name<\/label>/, output_buffer
+    assert_match /<input id="user_name" name="user\[name\]" type="text" value="#{user_form.name}" \/>/, output_buffer
+    assert_match /<label for="user_age">Age<\/label>/, output_buffer
+    assert_match /<input id="user_age" name="user\[age\]" type="number" value="#{user_form.age}" \/>/, output_buffer
+    assert_match /<label for="user_gender">Gender<\/label>/, output_buffer
+    assert_match /<select id="user_gender" name="user\[gender\]">/, output_buffer
+    assert_match /<option selected="selected" value="0">Male<\/option>/, output_buffer
+    assert_match /<option value="1">Female<\/option>/, output_buffer
+    assert_match /<\/select>/, output_buffer
+
+    assert_match /<label for="user_email_attributes_address">Address<\/label>/, output_buffer
+    assert_match /<input id="user_email_attributes_address" name="user\[email_attributes\]\[address\]" type="text" value="#{user_form.email.address}" \/>/, output_buffer
+    assert_match /<input id="user_email_attributes_id" name="user\[email_attributes\]\[id\]" type="hidden" value="#{user_form.email.id}" \/>/, output_buffer
 
     assert_match /<input name="commit" type="submit" value="Update User" \/>/, output_buffer
   end
